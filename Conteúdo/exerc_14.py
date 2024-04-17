f14881e584342e47e154085e3a98afc86c98ba65
K <97068125+Gabriel-Sousa-Amorim@users.noreply.github.com>
2024-04-16 23:53:00 -0300

    Create exerc_14.py

---
+++ Conteúdo/exerc_14.py
@@ -0,0 +1,13 @@
+"""
+Problema Prático 8.10
+
+Lembre-se de que também podemos percorrer um contêiner Queue usando o padrão de laço contador (isto é, percorrendo os índices).
+
+>>> for i in range(len(q)):
+         print(q[i])
+>>> 3
+>>> 5
+>>> 7
+>>> 9
+Que operador sobrecarregado, além do operador de indexação, precisa ser implementado para poder percorrer um contêiner usando esse padrão?
+"""
