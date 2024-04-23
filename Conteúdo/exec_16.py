2eb3668bf19b91d2ce36906b87a0debe98d21e03
K <97068125+Gabriel-Sousa-Amorim@users.noreply.github.com>
2024-04-22 23:17:10 -0300

    Create exec_16.py

---
+++ Conteúdo/exec_16.py
@@ -0,0 +1,15 @@
+"""
+Problema Prático 10.2
+
+Use o pensamento recursivo para implementar a função recursiva saúde() que, sobre a entrada inteira n, exibe n strings 'Hip ' seguidos por Hurrah.
+>>> cheers(0) >>> Hurrah!!!
+>>> cheers(1) >>> Hip Hurrah!!!
+>>> cheers(4) >>> Hip Hip Hip Hip Hurrah!!!
+
+O caso básico da recursão deverá ser quando n é 0; sua função deverão, então, exibir Hurrah. Quando n > 1, sua função deverá exibir 'Hip ' e depois chamar recursivamente a si mesma sobre a entrada inteira n – 1.
+"""
+
+def cheers(n:int=0):
+    print(f"{'Hip ' * n}Hurrah!!!")
+
+cheers(4)
